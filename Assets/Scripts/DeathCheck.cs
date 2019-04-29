f2c226435235a65e4e498d7ec912928b95a4b207
anurajbh <anurajbh@users.noreply.github.com>
2019-04-29 16:06:19 -0400

    TODO- Finalize Level Design and add Victory condition

--- Assets/Scripts/DeathCheck.cs
+++ Assets/Scripts/DeathCheck.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class DeathCheck : MonoBehaviour
 {
     ReloadScene reload;
+    AudioSource aud;
     private void Awake()
     {
         reload = GetComponent<ReloadScene>();
+        aud = GetComponent<AudioSource>();
     }
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (!aud.isPlaying)
+        {
+            aud.Play();
+        }
+        Invoke("Restart", 0.5f);
+    }
+
+    private void Restart()
     {
         reload.LoadScene();
     }

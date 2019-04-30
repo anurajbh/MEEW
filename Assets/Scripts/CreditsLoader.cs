e1abc39a9c1f70b5daca7b0249850683f513c967
anurajbh <anurajbh@users.noreply.github.com>
2019-04-29 22:12:31 -0400

    Added End Credits

--- Assets/Scripts/CreditsLoader.cs
+++ Assets/Scripts/CreditsLoader.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreditsLoader : MonoBehaviour
 {
     AudioSource aud;
+    Scene scene;
+    int sceneIndex;
     private void Awake()
     {
+        scene = SceneManager.GetActiveScene();
+        sceneIndex = scene.buildIndex;
         aud = GetComponent<AudioSource>();
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -16,6 +21,11 @@ public class CreditsLoader : MonoBehaviour
         {
             aud.Play();
         }
+        Invoke("LoadCredits", 2f);
+    }
+    void LoadCredits()
+    {
+        SceneManager.LoadScene(sceneIndex + 1);
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
